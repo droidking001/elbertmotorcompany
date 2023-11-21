92b03235584be8c948726cf1495da804391b8835
droidking001 <droidking001@gmail.com>
2023-11-21 07:34:28 -0600

    bug fixes

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Contact from "@/components/Contact";
-import Cover from "@/components/Cover";
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
 import { Project } from "@/types/Project";
--- types/Project.ts
+++ types/Project.ts
@@ -1,5 +1,12 @@
 import { PortableTextBlock } from "sanity";
 
+export type Image = {
+  asset: {
+    url: string;
+  };
+  alt: string;
+};
+
 export type Project = {
   _id: string;
   _createdAt: Date;
@@ -7,7 +14,8 @@ export type Project = {
   make: string;
   model: string;
   slug: string;
-  image: string;
+  image: Image[];
+  url: string;
   miles: string;
   price: string;
   details: PortableTextBlock[];
